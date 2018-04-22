fcb2e6847e70b49a89b0890e0f2dee1bc1095613
Gustav Wennerblom <gustav.wennerblom@gmail.com>
2018-04-22 23:31:55 +0200

    Multiple enhancements

--- traflab-skill.py
+++ traflab-skill.py
@@ -1,32 +1,42 @@
-from flask_ask import Ask, question, statement
+from flask_ask import Ask, question, statement, session
 from flask import Flask, render_template
 from traflab2 import Trafiklab
 from glogger.gLogger import GLogger
+import json
+from collections import defaultdict
+from DBHelper import DBHelper
 
 log = GLogger(name='traflab-alexa-main').get_logger()
 
 app = Flask(__name__)
 ask = Ask(app, '/')
-
-SITE_IDS = {'sickla kaj': '1550'}       # TODO: Move out to separate file
+db = DBHelper()
 
 log.info("Log enabled")
 
 
+# Build a dict of site id's to use when querying the main Trafiklab API
+SITE_IDS = defaultdict(set)
+with open('sl_Site_20180422-2113.json') as f:
+    full_map = json.load(f)
+    for site in full_map['ResponseData']['Result']:
+        SITE_IDS[site['SiteName'].lower()].add(site['SiteId'])
+        # SITE_IDS[site['StopPointName']].add((site['StopAreaNumber'],site['StopAreaTypeCode']))
+    log.info('Built reference of SITE_IDs ({} entries)'.format(len(SITE_IDS)))
+
+
 @ask.launch
 def launch_skill():
-    log.info("Skill launched")
-    return question(render_template('welcome'))
+    user_id = session.user.userId
+    log.info("Skill launched for userId {}".format(user_id))
+    departure_site = db.get_default_site(user_id)
+    return question(render_template('welcome', departure_site=departure_site))
 
 
-# TODO: Find a more graceful way of handling default. Possibly a combo of https://www.trafiklab.se/api/sl-platsuppslag/dokumentation and https://developer.amazon.com/docs/custom-skills/device-address-api.html#getAddress
-@ask.intent('OneShotDepartureIntent',
-            default={'origin': 'sickla kaj'})
-def one_shot_departure(origin, direction, mode):
-    log.info("OneShotDepartureIntent got origin={}, direction={}, mode={}".format(origin, direction, mode))
-    if origin.lower() not in SITE_IDS:
-        return supported_origins()
-    return _make_trafiklab_request(origin=origin, direction=direction, mode=mode)
+@ask.intent('OneShotDepartureIntent')
+def one_shot_departure(direction, mode):
+    log.info("OneShotDepartureIntent got direction={}, mode={}".format(direction, mode))
+    return _make_trafiklab_request(origin=db.get_default_site(session.user.userId), direction=direction, mode=mode)
 
 
 @ask.intent('DialogDepartureIntent')
@@ -36,25 +46,72 @@ def dialog_departure():
 
 @ask.intent('SupportedOriginsIntent')
 def supported_origins():
-    log.info('Got into supported endpoints')
+    log.info('Got into supported origins')
     origins = ', '.join(SITE_IDS.keys())
     list_origins_utterance = render_template('list_origins', origins=origins)
     return question(list_origins_utterance)
+
+@ask.intent('MyAddressIntent')
+def check_my_address():
+    log.info('Stating device address')
+    for key, value in get_device_address().items():
+        log.info('Key: {}, Value: {}'.format(key, value))
+    return statement(render_template('dev_confirmation', func='check_my_address'))
 
 # TODO: Fix a function for setting a default origin
 
 
 def _make_trafiklab_request(origin, mode, direction):   # TODO: Handle direction argument
     assert origin in SITE_IDS
-    trafiklab = Trafiklab(siteid=SITE_IDS[origin])
-    departure = trafiklab.get_trams()[0]
+    trafiklab = Trafiklab(siteid=SITE_IDS[origin].pop())        # TODO: Handle multiple sites
+    if mode == 'tram':
+        departures = trafiklab.get_trams()
+    elif mode == 'bus':
+        departures = trafiklab.get_buses()
+    else:
+        raise ValueError('Got unsupported mode: "{}"'.format(mode))
+
+    if not departures:   # get_x method has returned empty list
+        return statement(render_template('sl_error'))
+
+    departure = departures[0]
+
+    """
     log.info("Returning departure_info with mode={}, display_time={}, destination={}"
              .format(mode, departure.display_time, departure.destination))
     statement_utterance = render_template('departure_info',
                                           mode=mode,
                                           display_time=departure.display_time,
                                           destination=departure.destination)
+    """
+    # TODO: Handle full list of departures (Below is untested)
+    all_departures_text = [render_template('departure_info',
+                                           mode=mode,
+                                           line_number=departure.line_number,
+                                           display_time=departure.display_time,
+                                           destination=departure.destination)
+                           for departure in departures]
+    
+    statement_utterance= '...'.join(all_departures_text)
     return statement(statement_utterance)
+
+
+def get_device_address():   # TODO: Test with a real device
+    from flask_ask import context
+    import requests
+    from urllib.parse import urljoin
+    device_id = context.System.device.deviceId
+    api_access_token = context.System.apiAccessToken
+    api_endpoint = context.System.apiEndpoint
+    api_path = '/v1/devices/{deviceId}/settings/address'
+    log.info("Identified device as bearing id {}".format(device_id))
+    log.info("Token for API access is {}".format(api_access_token))
+
+    headers = dict(Authorization='Bearer {}'.format(api_access_token))
+    address_endpoint = urljoin(api_endpoint, api_path)
+    result = requests.get(address_endpoint.format(deviceId=device_id), headers=headers)
+    address_json = result.json()
+    return address_json
 
 
 if __name__ == '__main__':
--- traflab2.py
+++ traflab2.py
@@ -1,14 +1,19 @@
 from credentials import trafiklab_key
 import requests
 from collections import namedtuple
+from pprint import pprint
+from datetime import datetime
+from glogger.gLogger import GLogger
+import json
 
 
 class Trafiklab(object):
     SITE_ID_SICKLAKAJ="1550"
     BASE_URL = "http://api.sl.se/api2/realtimedeparturesV4.{schema}?key={key}&siteid={siteid}&timewindow={timewindow}"
+    # TODO: Fix logging
 
     def get_full_response(self, url):
-        #Returns a dict from the JSON provided by the Trafiklab API, given a compliant REST call
+        # Returns a dict from the JSON provided by the Trafiklab API, given a compliant REST call
         import requests
         response=requests.get(url)
         return response.json()
@@ -17,8 +22,9 @@ class Trafiklab(object):
         # Returns a namedtuple of tram destinations and departure times (values, as displayed on boards)
         response = requests.get(self.endpoint).json()
         trams = []
-        Departure = namedtuple('Departure', ['destination', 'display_time'])
+        Departure = namedtuple('Departure', ['destination', 'display_time'])    # TODO: Replicate for buses w line no
         for dept in response["ResponseData"]["Trams"]:
+            self.log("Got {}".format(dept))
             trams.append(Departure(dept["Destination"], dept["DisplayTime"]))
         return trams
 
@@ -26,9 +32,9 @@ class Trafiklab(object):
         # Returns a namedtuple of bus destinations and departure times (values, as displayed on boards)
         response = requests.get(self.endpoint).json()
         buses = []
-        Departure = namedtuple('Departure', ['destination', 'display_time'])
+        Departure = namedtuple('Departure', ['destination', 'display_time', 'line_number'])
         for dept in response["ResponseData"]["Buses"]:
-            buses.append(Departure(dept["Destination"], dept["DisplayTime"]))
+            buses.append(Departure(dept["Destination"], dept["DisplayTime"], dept["LineNumber"]))
         return buses
 
     def set_endpoint(self, schema, key, site_id, time_window):
@@ -36,6 +42,7 @@ class Trafiklab(object):
         return endpoint
 
     def __init__(self, schema='json', siteid=None, timewindow='30'):
+        self.log = GLogger(name=__name__).get_logger()
         self.API_KEY = trafiklab_key.key
         if not siteid:
             self.home_station = self.SITE_ID_SICKLAKAJ
@@ -45,12 +52,44 @@ class Trafiklab(object):
                                              key=self.API_KEY,
                                              siteid=self.home_station,
                                              timewindow=timewindow)
+        self.log.info("Trafiklab main object initiated")
 
+class StopPointFetcher(object):     #TODO: Change name to reflect expanded scope
+    """
+    This class interacts with the Stops and Lines directory API https://www.trafiklab.se/api/sl-hallplatser-och-linjer-2
+    """
+    def __init__(self, schema='json', granularity='StopPoint'):  #TODO: Use json parameter in endpoint string formatter
+        self.log = GLogger(name=__name__).get_logger()
+        self.granularity = granularity
+        self.API_KEY = trafiklab_key.key_stops_and_lines
+        self.endpoint = ('http://api.sl.se/api2/LineData.{1}?model={2}&key={0}'
+                         .format(self.API_KEY, schema, granularity))
+
+    def get_stop_points(self):
+        result = requests.get(self.endpoint).json()
+        self.log.info("Queried for {}".format(self.granularity))
+        return result
+
+    def download_stop_points(self):
+        datetime_string = datetime.now().strftime('%Y%m%d-%H%M')
+        filename = 'sl_{1}_{0}.json'.format(datetime_string, self.granularity)
+        with open(filename,'w') as f:
+            f.write(json.dumps(self.get_stop_points()))
+            self.log.info("{1} data written to {0}".format(filename, self.granularity))
+        return 200
+
+# class SiteFetcher(object):
+#     def __init__(self, schema='json'):
+#         self.log = GLogger(name="SiteFetcherLog: " + __name__).get_logger()
+#         self.API_KEY = trafiklab_key
 
 if __name__ == '__main__':
     trafiklab = Trafiklab()
     result_trams = trafiklab.get_trams()
-    result_buses = trafiklab.get_buses()
+    # result_buses = trafiklab.get_buses()
     from pprint import pprint
     pprint(result_trams)
-    pprint(result_buses)
+    # pprint(result_buses)
+    # spf = StopPointFetcher(granularity='Site')
+    # pprint(spf.get_stop_points())
+    # print(spf.download_stop_points())
